cdaf83fba517b4d298bbf7277abd74405e0e3019
Oevin7 <131039022+Oevin7@users.noreply.github.com>
2024-06-09 18:50:35 -0400

    Added the remove feature. You are now able to call the remove command, to remove tasks from your list.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,28 @@ fn add_to_list(task : Todo, list: &mut Vec<Todo>) {
     list.push(task);
 }
 
+fn remove_task(task_to_remove : &str, path_to_file : &str, path : bool) {
+
+    let mut list = match read_and_return(path_to_file, path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Could not read the file due to {}", e);
+            return
+        }
+    };
+
+    if let Some(data) = list.iter().position(|task| task.get_task().trim() == task_to_remove) {
+        list.remove(data);
+    } else {
+        eprintln!("Task was not found in the list.")
+    }
+
+    if let Err(e) = write_file(&mut list, path_to_file) {
+        eprintln!("Could not write to file: {}. Error: {}", path_to_file, e);
+    }
+
+}
+
 fn create_task(task : &str, importance : i32) -> Todo {
     let new_task = Todo::new(task.parse().unwrap(), false, importance);
     new_task
@@ -63,7 +85,6 @@ fn handle_command(command : &str, todo_list: Vec<Todo>, file_path : &str, path :
     loop {
         match command {
             "list" => {
-                let list_ref = &list;
                 list_tasks(file_path, path).expect("Could not get data from the file.");
                 break;
             }
@@ -97,13 +118,13 @@ fn handle_command(command : &str, todo_list: Vec<Todo>, file_path : &str, path :
                 let input = input().expect("Could not unwrap String");
 
                 if input.trim() == "exit" || input.trim() == "e" {
-                    write_file(&mut list, file_path, path).expect("Could not parse the file");
+                    write_file(&mut list, file_path).expect("Could not parse the file");
                     break;
                 }
 
             }
             "help" => {
-                println!("\
+                println!("\n\
                 \t\tlist: Lists the tasks that are currently on your list. Uncompleted and
                 Completed will show up unless you use a filter, or when you exit the program.
                 Exiting automatically cleans up completed tasks.
@@ -112,27 +133,21 @@ fn handle_command(command : &str, todo_list: Vec<Todo>, file_path : &str, path :
                 modified to change their importance. You can also filter these tasks later to only
                 view the ones you need.
 
+                remove: Removes a task from your list. All tasks marked completed are automatically
+                removed when the program exits. However, you can preemptively remove tasks if you'd
+                like!
                 ");
 
                 break;
 
             }
-            "find" => {
-                loop {
-                    println!("What task would you like to find?");
-                    let task_to_find = input().unwrap();
+            "remove" => {
+                println!("Please input the task you would like to remove: ");
+                let task_to_remove = input().expect("Couldn't get user input");
 
-                    let task = find_task(task_to_find.trim(), file_path, path);
+                remove_task(task_to_remove.trim(), file_path, path);
+                break;
 
-                    if task.is_none() {
-                        println!("That task was not found in the list. Please try again!");
-                    } else {
-                        let data_in_task = task.unwrap();
-                        println!("{:?}", data_in_task.get_task().trim());
-                        break
-                    }
-                    break
-                }
             },
             _ => {
                 panic!("NO FEATURES HERE!!!! ABORT, ABORT! TO LAZY TO PROPERLY HANDLE!");
@@ -141,26 +156,11 @@ fn handle_command(command : &str, todo_list: Vec<Todo>, file_path : &str, path :
     }
 }
 
-fn write_file(list : &mut Vec<Todo>, file_path : &str, path : bool) -> Result<(), io::Error> {
+fn write_file(list : &Vec<Todo>, file_path : &str) -> Result<(), io::Error> {
 
     let mut data = String::new();
-    let mut existing_tasks : Vec<Todo> = Vec::new();
-    
-    if path {
-        let mut open_file = File::open(file_path)?;
-        open_file.read_to_string(&mut data)?;
 
-        match serde_json::from_str(&data) {
-            Ok(tasks) => existing_tasks = tasks,
-            Err(e) => {
-                if e.classify() != serde_json::error::Category::Eof {
-                    return Err(e.into());
-                }
-            }
-        }
-    }
-
-    existing_tasks.append(list);
+    let existing_tasks = list.clone();
 
     let serialized_data = serde_json::to_string_pretty(&existing_tasks)?;
 
